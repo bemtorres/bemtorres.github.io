7d963eb46d0819a7a6bc57224065b8a809ca330b
Benjamin <benja.mora.torres@gmail.com>
2018-11-12 01:42:51 -0300

    WPA bemtorres

--- sw.js
+++ sw.js
@@ -1,15 +1,30 @@
-var CACHE_NAME = 'my-site-cache-v1';
+var CACHE_NAME = 'bemtorres-cache-v1';
 var urlsToCache = [
   '/',
-  '/css/bootstrap.min.css',
-  '/css/one-page-wonder.css',
-  '/js/app.js',
-  '/img/01.jpg',
-  '/img/02.jpg',
-  '/img/03.jpg',
+  '/dist/img/avatar/avatar-1.jpg',
+  '/dist/modules/bootstrap/css/bootstrap.min.css',
+  '/dist/modules/ionicons/css/ionicons.min.css',
+  '/dist/modules/fontawesome/web-fonts-with-css/css/fontawesome-all.min.css',
+  '/dist/modules/summernote/summernote-lite.css',
+  '/dist/css/demo.css',
+  '/dist/css/style.css',
+  '/dist/modules/jquery.min.js',
+  '/dist/modules/popper.js',
+  '/dist/modules/tooltip.js',
+  '/dist/modules/bootstrap/js/bootstrap.min.js',
+  '/dist/modules/nicescroll/jquery.nicescroll.min.js',
+  '/dist/modules/scroll-up-bar/dist/scroll-up-bar.min.js',
+  '/dist/js/sa-functions.js',
+  '/dist/modules/summernote/summernote-lite.js',
+  '/idist/js/scripts.js',  
+  '/dist/js/custom.j',
+  '/dist/img/news/img01.jpg',
+  '/dist/img/news/img07.jpg',  
+  '/dist/img/news/img08.jpg',
   'https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900',
   'https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i'
 ];
+
 
 self.addEventListener('install', function(event) {
   // Perform install steps
