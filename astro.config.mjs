0ba12ab1ca016e1e9adb75530e1b7c8f3bca7fd6
Bemtorres <benja.mora.torres@gmail.com>
2025-07-29 22:17:07 -0400

    new

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://bemtorres.github.io"
+  site: "https://www.profebenja.lat/"
 });
